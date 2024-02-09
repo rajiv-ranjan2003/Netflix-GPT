9783629d64d1cfee5a449b21a66dd5c6a1c9c372
Rajiv Ranjan <rajivranjan1131@gmail.com>
2024-02-10 00:26:28 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,8 @@ import { IMG_CDN } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN + posterPath} />
+    <div className="w-36 md:w-48 pr-4 hover:scale-150 transition duration-200 ease-out hover:ease-in rounded-md hover:m-9  cursor-pointer  hover:overflow-hidden ">
+      <img alt="Movie Card" src={IMG_CDN + posterPath} className="rounded-md" />
     </div>
   );
 };
